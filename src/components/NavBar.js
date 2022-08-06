53bb4b4651da3c5a0b2d203a0c7ddd7b84030082
= <dcnation1000000@gmail.com>
2022-08-06 11:32:48 -0400

    make navbar responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react'
-import { Spacer, Heading, HStack } from '@chakra-ui/layout'
+import { Spacer, Heading, HStack, Flex } from '@chakra-ui/layout'
 import { IconButton } from "@chakra-ui/button";
 import { FaGithub, FaLinkedin } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
@@ -9,17 +9,19 @@ import ThemeSwitch from './ThemeSwitch.js'
 function NavBar() {
   return (
     <HStack w="98%">
-      <HStack mr="1000" mt="1.5" >
-        <Heading size="md" fontWeight='semibold'>Anik Singha</Heading>
-        <a href="https://www.linkedin.com/in/anik-singha/" target='_blank' rel="noreferrer">
-          <IconButton icon={<FaLinkedin />} isRound='true' mt='1'></IconButton>
-        </a>
-        <a href="https://github.com/AnikSingha" target="_blank" rel="noreferrer">
-          <IconButton icon={<FaGithub />} isRound='true' mt='1'></IconButton>
-        </a>
-      </HStack>
+      <Flex>
+        <HStack>
+          <Heading size="md" fontWeight='semibold'>Anik Singha</Heading>
+          <a href="https://www.linkedin.com/in/anik-singha/" target='_blank' rel="noreferrer">
+            <IconButton icon={<FaLinkedin />} isRound='true' mt='1'></IconButton>
+          </a>
+          <a href="https://github.com/AnikSingha" target="_blank" rel="noreferrer">
+            <IconButton icon={<FaGithub />} isRound='true' mt='1'></IconButton>
+          </a>
+        </HStack>
+      </Flex>
       <Spacer/>
-      <Breadcrumb spacing='7px' separator=' ' mt={1} ml={1150}>
+      <Breadcrumb spacing='7px' separator=' ' mt={1}>
         <BreadcrumbItem isCurrentPage>
           <BreadcrumbLink fontWeight='semibold' as={Link} to='/'>Home</BreadcrumbLink>
         </BreadcrumbItem>
--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import myGif from './images/terminal.gif';
+import { useMediaQuery } from '@chakra-ui/react'
 
 export default function Terminal() {
+  let style = {}
+  const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)")
+  if (isNotSmallerScreen ? style = {width: "auto", maxWidth: 800, height: 'auto',}: style={display: 'none'})
   return (
-      <img height='700' width='700' src={myGif} alt="my-gif" />
+      <img style={style} src={myGif} alt="my-gif" />
   )
 }
