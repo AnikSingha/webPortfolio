bfe0ad3953374158ad3849b76d8a4e556589ac68
= <dcnation1000000@gmail.com>
2022-08-05 20:46:31 -0400

    last attempt

--- src/index.js
+++ src/index.js
@@ -11,10 +11,12 @@ const config = {
 
 const theme = extendTheme({ config })
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode}></ColorModeScript>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
