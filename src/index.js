c5231321bb2971d2e4052e2527c3c547b9d9e14d
= <dcnation1000000@gmail.com>
2022-08-05 20:55:10 -0400

    last effort

--- src/index.js
+++ src/index.js
@@ -4,19 +4,18 @@ import App from './App';
 import { ChakraProvider, ColorModeScript} from "@chakra-ui/react"
 import { extendTheme } from "@chakra-ui/react"
 
-const config = {
-  initialColorMode: "dark",
-  useSystemColorMode: false,
-}
-
-const theme = extendTheme({ config })
+const theme = extendTheme({
+  config: {
+    useSystemColorMode: true,
+    initialColorMode: "dark"
+  }
+});
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode}></ColorModeScript>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
