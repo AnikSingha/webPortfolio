c0f5c5b8843a81f41197a51430c7f667e29518f9
= <dcnation1000000@gmail.com>
2022-08-07 15:44:07 -0400

    Finalize mobile

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -1,18 +1,12 @@
-import NavBar from '../components/NavBar.js';
-import Footer from '../components/Footer.js';
 import EmailProjectCard from '../components/projectCards/EmailProjectCard.js';
-import { HStack, VStack, Flex, Text} from '@chakra-ui/layout'
+import { HStack, VStack, Flex } from '@chakra-ui/layout'
 import GameProjectCard from '../components/projectCards/GameProjectCard.js';
 import APICard from '../components/projectCards/APICard.js'
 import TitanicProjectCard from '../components/projectCards/TitanicProjectCard'
-import { useMediaQuery } from '@chakra-ui/react'
 
 export default function Projects() {
-    const [isNotSmallerScreen] = useMediaQuery("(min-width:1025px)")
     return (
-        <VStack>
-            <NavBar/>
-            <Text fontSize="5xl" fontWeight="bold" >My Projects</Text>
+        <div>
             <Flex alignItems='space-around'>
                 <VStack>
                     <HStack spacing='100px'>
@@ -26,7 +20,6 @@ export default function Projects() {
                 </VStack>
             </Flex>
             <br/><br/>
-            <Footer/>
-        </VStack>
+        </div>
     )
 }
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,12 +1,19 @@
+import NavBar from '../components/NavBar.js'
+import Footer from '../components/Footer.js';
 import ProjectContainer from '../components/ProjectContainer.js'
 import MobileProjectContainer from '../components/MobileProjectContainer.js'
 import { useMediaQuery } from '@chakra-ui/react'
+import { VStack, Text } from '@chakra-ui/layout'
+
 
 export default function Projects() {
     const [isNotSmallerScreen] = useMediaQuery("(min-width:1025px)")
     return (
-        <div>
+        <VStack>
+            <NavBar/>
+            <Text fontSize="5xl" fontWeight="bold" >My Projects</Text>
             {isNotSmallerScreen ? <ProjectContainer/> : <MobileProjectContainer/>}
-        </div>
+            <Footer/>
+        </VStack>
     )
 }
