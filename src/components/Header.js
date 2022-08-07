9079ead4cdaaebe04a8d5d21545e820943b515cb
= <dcnation1000000@gmail.com>
2022-08-07 11:30:29 -0400

    make home page mobile friendly

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,10 @@ function Header() {
     return (
         <Stack>
             <HStack mt='75' ml='50'>
-                <Flex>
+                <Flex alignItems='center' justifyContent='space-around'>
                 <Box mt={isNotSmallerScreen ? "0" : 16} mb="75">
-                    <Text fontSize="5xl" fontWeight="semibold" mr="400">Hi, I'm</Text>
-                    <Text fontSize="7xl" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.500)" bgClip='text'>Anik Singha</Text>
+                    <Text fontSize={isNotSmallerScreen ? "5xl" : "4xl"} fontWeight="semibold" mr={isNotSmallerScreen ? "400" : "0"}>Hi, I'm</Text>
+                    <Text fontSize={isNotSmallerScreen ? "7xl" : "5xl"} fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.500)" bgClip='text'>Anik Singha</Text>
                     <Text fontWeight='semibold' >A Full-Stack developer and CS student at Hunter College</Text>
                     <a href={resume} download><Button mt={5} colorScheme="blue">My Resume</Button></a>
                 </Box>
--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -4,8 +4,8 @@ import { useMediaQuery } from '@chakra-ui/react'
 
 export default function Terminal() {
   let style = {}
-  const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)")
-  if (isNotSmallerScreen ? style = {width: "auto", maxWidth: 800, height: 'auto'}: style={display: 'none'})
+  const [isNotSmallerScreen] = useMediaQuery("(min-width:800px)")
+  if (isNotSmallerScreen ? style = {width: "auto", maxWidth: 700, height: 'auto'}: style={display: 'none'})
   return (
       <img style={style} src={myGif} alt="my-gif" />
   )
