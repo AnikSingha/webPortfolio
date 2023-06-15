9f1b67a84d24d16c5383f0275371caf3d908ecc9
= <dcnation1000000@gmail.com>
2023-06-15 19:47:02 -0400

    move resume to public folder

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import { Stack, Box, Text, HStack, Flex} from '@chakra-ui/layout';
 import { useMediaQuery } from 'react-responsive'
 import { Button } from '@chakra-ui/button';
-import resume from '../Anik-Singha-Resume.pdf'
 import Terminal from './Terminal.js'
 import React from 'react'
 
@@ -17,7 +16,7 @@ function Header() {
                     <Text fontSize={isNotSmallerScreen ? "5xl" : "5xl"} fontWeight="semibold" mr={isNotSmallerScreen ? "400" : "0"}>Hi, I'm</Text>
                     <Text fontSize={isNotSmallerScreen ? "7xl" : "5xl"} fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.500)" bgClip='text'>Anik Singha</Text>
                     <Text fontWeight='semibold' >A Full-Stack developer and CS student at Hunter College</Text>
-                    <a href={resume} download="Anik-Singha-Resume.pdf" target="_blank" rel="noopener noreferrer"><Button mt={5} colorScheme="blue">My Resume</Button></a>
+                    <a href={'../../public/Anik-Singha-Resume.pdf'} download="Anik-Singha-Resume.pdf" target="_blank" rel="noopener noreferrer"><Button mt={5} colorScheme="blue">My Resume</Button></a>
                 </Box>
                 </Flex>
                 <Terminal/>
