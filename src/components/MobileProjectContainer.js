d8c4fc2b13e0f26c629f3b3256708b00d9d50948
Anik Singha <aniksingha@outlook.com>
2024-01-11 11:56:55 -0500

    fix mobile version

--- src/components/MobileProjectContainer.js
+++ src/components/MobileProjectContainer.js
@@ -4,12 +4,14 @@ import GameProjectCard from '../components/projectCards/GameProjectCard.js';
 import APICard from '../components/projectCards/APICard.js'
 import TitanicProjectCard from '../components/projectCards/TitanicProjectCard'
 import ChatProjectCard from '../components/projectCards/ChatProjectCard.js'
+import WhisperProjectCard from './projectCards/WhisperProjectCard.js';
 
 export default function Projects() {
     return (
         <div>
             <Flex alignItems='space-around'>
                 <VStack>
+                    <WhisperProjectCard/>
                     <EmailProjectCard/>
                     <ChatProjectCard/>
                     <APICard/>
