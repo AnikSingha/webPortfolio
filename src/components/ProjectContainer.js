9716f4f8f275c0149e7bf5a3aa88cb1b9677a6a4
Anik Singha <aniksingha@outlook.com>
2024-09-15 21:32:03 -0400

    get rid of project sections

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -1,57 +1,40 @@
-import React from 'react';
 import EmailProjectCard from '../components/projectCards/EmailProjectCard.js';
-import { HStack, VStack, Flex, Box, Heading } from '@chakra-ui/react';
-import { useMediaQuery } from 'react-responsive';
+import { HStack, VStack, Flex } from '@chakra-ui/layout'
+import { useMediaQuery } from 'react-responsive'
 import GameProjectCard from '../components/projectCards/GameProjectCard.js';
-import APICard from '../components/projectCards/APICard.js';
-import TitanicProjectCard from '../components/projectCards/TitanicProjectCard';
-import ChatProjectCard from '../components/projectCards/ChatProjectCard.js';
+import APICard from '../components/projectCards/APICard.js'
+import TitanicProjectCard from '../components/projectCards/TitanicProjectCard'
+import ChatProjectCard from '../components/projectCards/ChatProjectCard.js'
 import WhisperProjectCard from './projectCards/WhisperProjectCard.js';
 import JeopardyProjectCard from './projectCards/Jeopardy.js';
-import PasswordManagerProjectCard from './projectCards/PasswordManager';
+import PasswordManagerProjectCard from './projectCards/PasswordManager'
 
 export default function Projects() {
     const isScreenBig = useMediaQuery({ 
         query: '(min-height: 900px)' 
-    });
+    })
 
     return (
         <div>
-            <Flex direction="column" alignItems="center">
-                <VStack spacing="40px" style={{ marginTop: isScreenBig ? '30px' : '0' }}>
-                    
-                    {/* Advanced Section */}
-                    <Box>
-                        <Heading size="lg" mb="20px">Advanced</Heading>
-                        <HStack spacing="100px" mb="30px">
-                            <PasswordManagerProjectCard/>
-                            <JeopardyProjectCard/>
-                            <WhisperProjectCard/>
-                        </HStack>
-                    </Box>
-
-                    {/* Intermediate Section */}
-                    <Box>
-                        <Heading size="lg" mb="20px">Intermediate</Heading>
-                        <HStack spacing="100px" mb="30px">
-                            <ChatProjectCard/>
-                            <APICard/>
-                            <EmailProjectCard/>
-                        </HStack>
-                    </Box>
-
-                    {/* Beginner Section */}
-                    <Box>
-                        <Heading size="lg" mb="20px">Beginner</Heading>
-                        <HStack spacing="100px" mb="30px">
-                            <TitanicProjectCard/>
-                            <GameProjectCard/>
-                        </HStack>
-                    </Box>
-
+            <Flex alignItems='space-around'>
+                <VStack  style={{ marginTop: isScreenBig ? '30px' : '0' }}>
+                    <HStack spacing='100px'>
+                        <PasswordManagerProjectCard/>
+                        <JeopardyProjectCard/>
+                        <WhisperProjectCard/>
+                    </HStack>
+                    <HStack spacing='100px'>
+                        <ChatProjectCard/>
+                        <APICard/>
+                        <EmailProjectCard/>
+                    </HStack>
+                    <HStack spacing='100px'>
+                        <TitanicProjectCard/>
+                        <GameProjectCard/>
+                    </HStack>
                 </VStack>
             </Flex>
             <br/><br/>
         </div>
-    );
+    )
 }
